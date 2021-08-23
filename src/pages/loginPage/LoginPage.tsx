0b0f755b0c8b2c33f1d199231ed73bf26827688f
Asad-Ishak <asad.ishak@tillpayments.com>
2021-08-23 14:13:45 +1000

    Reworked login page

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import React from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./Login.css";
 
-export const LoginPage = () => {
+const LoginForm = () => {
+  return (
+    <>
+      <Form>
+        <Form.Field>
+          <label>Username</label>
+          <input className="input" placeholder="Kenny123" />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input className="input" placeholder="Last Name" />
+        </Form.Field>
+        <Button class="button" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+      <div className="line">
+        <div className="devicer"></div>
+        <div>
+          <p>Or</p>
+        </div>
+        <div className="devicer"></div>
+      </div>
+
+      <div>
+        <h3>Don't have an account?</h3>
+        <Link to="/create-account">
+          <Button class="button" type="submit">
+            Create Account
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+const PageAnimation = () => {
+  return (
+    <div className="container">
+      <h2 className="title">Welcome to Link</h2>
+      <img className="img" src="" alt="" />
+    </div>
+  );
+};
+
+const Login = () => {
   return (
     <div>
-      
+      <Route path="/login">
+        <LoginForm />
+        <PageAnimation />
+      </Route>
     </div>
-  )
-}
+  );
+};
 
+export default Login;
