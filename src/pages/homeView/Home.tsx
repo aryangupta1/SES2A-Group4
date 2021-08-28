df24ea3825dae3dc09920e1350536b9417c5fa3c
Asad-Ishak <asad.ishak@tillpayments.com>
2021-08-29 03:04:00 +1000

    Refactored NavBar component for reusability

--- src/pages/homeView/Home.tsx
+++ src/pages/homeView/Home.tsx
@@ -1,70 +1,73 @@
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 import React, { useState } from "react";
-import { Button } from 'semantic-ui-react'
+import { Button } from "semantic-ui-react";
 import "./Home.css";
-import teamImage from "../../images/team-image.svg"
-import Navbar from '../../components/navBar/Navbar';
-
+import teamImage from "../../images/team-image.svg";
+import Navbar from "../../components/navBar/Navbar";
 // import React, { useState } from "react";
 // import { useHistory } from "react-router-dom";
 
 export const Home = () => {
-//   const history = useHistory();
-//   const [modalOpen, setModalOpen] = useState(false);
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [signUp, setSignUp] = useState(false);
-//   const [wrongUserPass, setWrongUserPass] = useState(false);
+  //   const history = useHistory();
+  //   const [modalOpen, setModalOpen] = useState(false);
+  //   const [username, setUsername] = useState("");
+  //   const [password, setPassword] = useState("");
+  //   const [signUp, setSignUp] = useState(false);
+  //   const [wrongUserPass, setWrongUserPass] = useState(false);
 
-//   const onUserChange = (val: any) => {
-//     setUsername(val.target.value);
-//     console.log(val.target.value);
-//   };
+  //   const onUserChange = (val: any) => {
+  //     setUsername(val.target.value);
+  //     console.log(val.target.value);
+  //   };
 
-//   const onPasswordChange = (val: any) => {
-//     setPassword(val.target.value);
-//     console.log(val.target.value);
-//   };
+  //   const onPasswordChange = (val: any) => {
+  //     setPassword(val.target.value);
+  //     console.log(val.target.value);
+  //   };
 
-//   const beginSignUp = () => {
-//     setSignUp(true);
-//   };
+  //   const beginSignUp = () => {
+  //     setSignUp(true);
+  //   };
 
-//   const endSignup = () => {
-//     setSignUp(false);
-//   };
+  //   const endSignup = () => {
+  //     setSignUp(false);
+  //   };
 
-//   const openModal = () => {
-//     setModalOpen(true);
-//   };
+  //   const openModal = () => {
+  //     setModalOpen(true);
+  //   };
 
-//   const closeModal = () => {
-//     setModalOpen(false);
-//   };
+  //   const closeModal = () => {
+  //     setModalOpen(false);
+  //   };
 
-//   const openWrongUserPass = () => {
-//     setWrongUserPass(true);
-//   };
-//   const closeWrongUserPass = () => {
-//     setWrongUserPass(false);
-//   };
+  //   const openWrongUserPass = () => {
+  //     setWrongUserPass(true);
+  //   };
+  //   const closeWrongUserPass = () => {
+  //     setWrongUserPass(false);
+  //   };
 
-//   const onClickLogin = () => {
-//     console.log("You submitted a password");
-//   };
+  //   const onClickLogin = () => {
+  //     console.log("You submitted a password");
+  //   };
   return (
     <div className="background">
-      <Navbar children = {} />
+      <Navbar children={["about", "login", "create-account"]} />
       <div className="horizontal">
         <div className="leftContainer">
-          <h2 className="h2">Making Group Work <span className="easy">Easy</span></h2>
+          <h2 className="h2">
+            Making Group Work <span className="easy">Easy</span>
+          </h2>
           <h3 className="h3">We streamline the group making process</h3>
-          <Button style={{backgroundColor: "#884AED", color: "white"}} circular>Get Started</Button>
+          <Button style={{ backgroundColor: "#884AED", color: "white" }} circular>
+            Get Started
+          </Button>
         </div>
         <div className="rightContainer">
           <img src={teamImage} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
