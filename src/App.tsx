704e148f03d5fcac4dbb0bfb76c9fea815843c52
aryangupta1 <aryangupta1@outlook.com>
2021-09-03 16:59:29 +1000

    Added route guards

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,38 @@ import { About } from "./pages/aboutPage/About";
 import Login from "./pages/loginPage/LoginPage";
 import StudentPage from "./pages/studentPage/studentPage";
 import { Preferences } from "./pages/Preferences/Preferences";
+import { GuardProvider, GuardedRoute } from 'react-router-guards';
 import AdminPage from "./pages/adminPage/adminPage";
+
+//In future need to create route checks specific for admin users
+const requireJWT = (to: any, from: any, next: any) => {
+  if(to.meta.auth){
+    const doesJWTExist: Boolean = (sessionStorage.getItem('JWT')) ? true: false;
+    if(doesJWTExist){
+      next();
+    }
+    next.redirect('/login')
+  }
+  else{
+    next();
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      <GuardProvider guards={[requireJWT]}>
         <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-          <Route path="/login" component={Login} />
-          <Route path="/student-page" component={StudentPage} />
-          <Route path="/Register" component={Register} />
-          <Route path="/preferences" component={Preferences} />
-          <Route path="/admin-page" component={AdminPage} />
+          <GuardedRoute path="/" component={Home} exact />
+          <GuardedRoute path="/about" component={About} />
+          <GuardedRoute path="/login" component={Login} />
+          <GuardedRoute path="/student-page" component={StudentPage} meta={{ auth: true }}/>
+          <GuardedRoute path="/admin-page" component={AdminPage} meta={{ auth: true }}/>
+          <GuardedRoute path="/Register" component={Register}/>
+          <GuardedRoute path="/preferences" component={Preferences} meta={{ auth: true }}/>
         </Switch>
+        </GuardProvider>
       </BrowserRouter>
     );
   }
