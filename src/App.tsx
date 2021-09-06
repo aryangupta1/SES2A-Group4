e42bed529b3c340da653018022125255fce2452a
Asad-Ishak <asad.ishak@tillpayments.com>
2021-09-07 01:51:14 +1000

    Added admin page with semi-functioning create assignment form

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,37 @@ import { About } from "./pages/aboutPage/About";
 import Login from "./pages/loginPage/LoginPage";
 import StudentPage from "./pages/studentPage/studentPage";
 import { Preferences } from "./pages/Preferences/Preferences";
-import { GuardProvider, GuardedRoute } from 'react-router-guards';
+import { GuardProvider, GuardedRoute } from "react-router-guards";
 import AdminPage from "./pages/adminPage/adminPage";
 
 //In future need to create route checks specific for admin users
 const requireJWT = (to: any, from: any, next: any) => {
-  if(to.meta.auth){
-    const doesJWTExist: Boolean = (sessionStorage.getItem('JWT')) ? true: false;
-    if(doesJWTExist){
+  if (to.meta.auth) {
+    const doesJWTExist: Boolean = sessionStorage.getItem("JWT") ? true : false;
+    if (doesJWTExist) {
       next();
     }
-    next.redirect('/login')
-  }
-  else{
+    next.redirect("/login");
+  } else {
     next();
   }
-}
+};
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <GuardProvider guards={[requireJWT]}>
-        <Switch>
-          <GuardedRoute path="/" component={Home} exact />
-          <GuardedRoute path="/about" component={About} />
-          <GuardedRoute path="/login" component={Login} />
-          <GuardedRoute path="/student-page" component={StudentPage} meta={{ auth: true }}/>
-          <GuardedRoute path="/admin-page" component={AdminPage} meta={{ auth: true }}/>
-          <GuardedRoute path="/Register" component={Register}/>
-          <GuardedRoute path="/preferences" component={Preferences} meta={{ auth: true }}/>
-        </Switch>
+        <GuardProvider guards={[requireJWT]}>
+          <Switch>
+            <GuardedRoute path="/" component={Home} exact />
+            <GuardedRoute path="/about" component={About} />
+            <GuardedRoute path="/login" component={Login} />
+            <GuardedRoute path="/student-page" component={StudentPage} meta={{ auth: true }} />
+            <GuardedRoute path="/admin-page" component={AdminPage} meta={{ auth: true }} />
+            <GuardedRoute path="/Register" component={Register} />
+            <GuardedRoute path="/adminPage" component={AdminPage} />
+            <GuardedRoute path="/preferences" component={Preferences} meta={{ auth: true }} />
+          </Switch>
         </GuardProvider>
       </BrowserRouter>
     );
--- src/pages/adminPage/adminPage.tsx
+++ src/pages/adminPage/adminPage.tsx
@@ -1,10 +1,87 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "semantic-ui-react";
 
 const AdminPage = () => {
-    const [email] = useState(sessionStorage.getItem('Email'));
-    return(
-        <h1>Welcome {email} to your admin dashboard</h1>
-    )
-}
+  const [owner] = useState(sessionStorage.getItem("Email"));
+  const [numberOfGroups, setGroupNumbers] = useState(10); // Not sure if this works but create state to get and set logins when user types their information
+  const [maxSizeOfGroup, setMaxSizeOfGroup] = useState(6);
+  const [assignmentName, setAssignmentName] = useState("");
+  const [studentPreferences, setPreferences] = useState<JSX.Element[]>();
+  const [studentSkills, setSkills] = useState<JSX.Element[]>();
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const studentPreferences = await fetch("http://localhost:8000/preferences");
+
+    const getPreferences = async () => {
+      // fetch preferences from backend & render them into form
+      const studentPreferences = await fetch("http://localhost:8000/preferences");
+      let prefs = await studentPreferences.json();
+      const prefRoles = [<option value="-">-</option>]; // initialise array with default value
+      for (const [key, value] of Object.entries(prefs)) {
+        // for each key and value inside object
+        prefRoles.push(<option value={key}>{`${value}`}</option>); // create a tag for the form with the values
+      }
+      setPreferences(prefRoles);
+    };
+    const getSkills = async () => {
+      // fetch skills from backend & render them into form
+      const studentSkills = await fetch("http://localhost:8000/skills");
+      let skills = await studentSkills.json();
+      const skillRoles = [<option value="-">-</option>]; // initialise array with default value
+      for (const [key, value] of Object.entries(skills)) {
+        // for each key and value inside object
+        skillRoles.push(<option value={key}>{`${value}`}</option>); // create a tag for the form with the values
+      }
+      setSkills(skillRoles);
+    };
+
+    try {
+      const createAssignment = await fetch("http://localhost:8000/assignments", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          owner,
+          numberOfGroups,
+          maxSizeOfGroup,
+          assignmentName,
+          studentPreferences,
+          studentSkills,
+        }),
+      });
+      const response = await createAssignment.json();
+      console.log(response);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Welcome {owner} to your admin dashboard</h1>
+      <h2>Create an assignment below!</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div className="form-input">
+          <label>Assignment Name</label>
+          <input className="input" onChange={(e) => setAssignmentName(e.target.value)} />
+        </div>
+        <div className="form-input">
+          <label>Number of Groups</label>
+          <input type="input" className="input" onChange={(e) => setGroupNumbers(parseInt(e.target.value))} />
+        </div>
+        <div className="form-input">
+          <label>Max size of each group</label>
+          <input className="input" onChange={(e) => setMaxSizeOfGroup(parseInt(e.target.value))} />
+        </div>
+
+        <div className="form-label">
+          <button className="button" type="submit">
+            Create Assignment!
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default AdminPage;
