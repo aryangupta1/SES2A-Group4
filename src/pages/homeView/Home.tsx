bfc92052e38d59030b1d155c4fb063ec043a510b
Justin <jjasa.2000@gmail.com>
2021-09-27 17:12:43 +1000

    fixing navbar issue

--- src/pages/homeView/Home.tsx
+++ src/pages/homeView/Home.tsx
@@ -3,21 +3,27 @@ import React, { useState } from "react";
 import { Button } from "semantic-ui-react";
 import styles from "./Home.module.css";
 import teamImage from "../../images/team-image.svg";
-/* import Navbar from "../../components/Navbar/Navbar"; */
+import { useHistory } from "react-router-dom";
+import Navbar from "../../components/NavBar/Navbar";
 // import React, { useState } from "react";
 // import { useHistory } from "react-router-dom";
 
 export const Home = () => {
+  const history = useHistory();
+
   return (
     <div className={styles.background}>
-      {/*       <Navbar children={["about", "login", "register"]} /> */}
+      <Navbar children={["about", "login", "register"]} />
       <div className={styles.horizontal}>
         <div className={styles.leftContainer}>
           <h2 className={styles.h2}>
             Making Group Work <span className="easy">Easy</span>
           </h2>
           <h3 className={styles.h3}>We streamline the group making process</h3>
-          <Button style={{ backgroundColor: "#884AED", color: "white", position: "inherit" }} circular>
+          <Button
+            style={{ backgroundColor: "#884AED", color: "white", position: "inherit" }}
+            onClick={() => history.push("/login")}
+          >
             Get Started
           </Button>
         </div>
