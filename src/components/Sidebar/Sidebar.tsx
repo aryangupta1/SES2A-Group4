612b008f6d0c5b12c42e68d6ab03e0d2204934ac
Julian-Ayoub <julianayoub1@outlook.com>
2021-10-03 20:06:23 +1100

    Make preference form dynamically generate object for backend

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import './Sidebar.css'
+import logo from '../../images/logo.svg'
+import assignmentImg from '../../images/assignments.svg'
+import 'semantic-ui-css/semantic.min.css'
+
+export const Sidebar = () => {
+
+    return (
+    <div className="wrapper">
+        
+    </div>
+    )
+}
