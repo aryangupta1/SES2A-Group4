a007690b1b84bb537ef6bef9d180a12b17144646
kensonlang28 <62220283+kensonlang28@users.noreply.github.com>
2021-10-21 12:11:43 +1100

    changed navbar colour

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -20,7 +20,7 @@ export const Navbar: React.FC<INavBarProps> = ({ children }) => {
       <div className={styles.navRightContainer}>
         <Button.Group>
           {children.map((buttonName) => (
-            <Button style={{ position: "inherit" }} onClick={() => history.push(`/${buttonName}`)}>
+            <Button style={{ position: "inherit", background:"linear-gradient(#387F9A, #D9E4EA)" }} onClick={() => history.push(`/${buttonName}`)}>
               {buttonName.charAt(0).toUpperCase() + buttonName.slice(1)}
             </Button>
           ))}
