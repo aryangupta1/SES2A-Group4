0f9f62efa18baaac32d543ed582227277aff6ab7
Justin <jjasa.2000@gmail.com>
2021-10-21 18:07:41 +1100

    commiting nav bar problems

--- src/pages/assignmentPage/assignmentPage.tsx
+++ src/pages/assignmentPage/assignmentPage.tsx
@@ -2,86 +2,89 @@ import { group } from "console";
 import { useEffect, useState } from "react";
 import { Button } from "semantic-ui-react";
 import AssignmentCard from "../../components/AssignmentCard/AssignmentCard";
-import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "../../components/NavBar/Navbar";
 import styles from "../adminPage/adminPage.module.css";
 import background from "../assignmentPage/assignmentPage.module.css";
 
 const AssignmentPage = () => {
-    const [groups, setGroups] = useState<[]>();
-    const [groupIds, setId] = useState<[]>();
-    const [assignment, setAssignment] = useState<any>();
-    //Get assignment name from query param
-    const assignmentName = window.location.search.replace('?', '');
-    const getAssignment = async () =>{
-        const email = sessionStorage.getItem('Email');
-        const assignmentRequest = await fetch(`http://localhost:8000/assignments/${email}/${assignmentName}`);
-        let assignmentInfo = await assignmentRequest.json();
-        //Save assignment in storage
-        sessionStorage.setItem(`${assignmentName}`, JSON.stringify(assignmentInfo));
-        setAssignment(JSON.stringify(assignmentInfo));
-        setGroups(await assignmentInfo[0].groups);
-        //Save group Ids for later
-        const groupIds: [] = [];
-        const id  = groups?.forEach((group)=>groupIds.push(group['id']));
-        setId(groupIds);
-    };
-    const getStudentInGroup = async () =>{
-        await groupIds?.forEach(async (id) => {
-            const getStudents = await fetch(`http://localhost:8000/assignments/${id}/${assignmentName}/students`)
-            const students = await getStudents.json();
-            sessionStorage.setItem(`${id}`, await students);
-        })
+  const [groups, setGroups] = useState<[]>();
+  const [groupIds, setId] = useState<[]>();
+  const [assignment, setAssignment] = useState<any>();
+  //Get assignment name from query param
+  const assignmentName = window.location.search.replace("?", "");
+  const getAssignment = async () => {
+    const email = sessionStorage.getItem("Email");
+    const assignmentRequest = await fetch(`http://localhost:8000/assignments/${email}/${assignmentName}`);
+    let assignmentInfo = await assignmentRequest.json();
+    //Save assignment in storage
+    sessionStorage.setItem(`${assignmentName}`, JSON.stringify(assignmentInfo));
+    setAssignment(JSON.stringify(assignmentInfo));
+    setGroups(await assignmentInfo[0].groups);
+    //Save group Ids for later
+    const groupIds: [] = [];
+    const id = groups?.forEach((group) => groupIds.push(group["id"]));
+    setId(groupIds);
+  };
+  const getStudentInGroup = async () => {
+    await groupIds?.forEach(async (id) => {
+      const getStudents = await fetch(`http://localhost:8000/assignments/${id}/${assignmentName}/students`);
+      const students = await getStudents.json();
+      sessionStorage.setItem(`${id}`, await students);
+    });
+  };
+  useEffect(() => {
+    getAssignment();
+    getStudentInGroup();
+  }, []);
+  const sort = async () => {
+    //Add students to assignment first
+    try {
+      const body = { assignmentName: assignmentName };
+      const addAllToAssignment = await fetch(`http://localhost:8000/addAllStudentsToAssignment`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      await addAllToAssignment.json().finally(async () => {
+        //Begin sorting
+        const sortAssignments = await fetch(`http://localhost:8000/sortAssignments`, {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        });
+        await sortAssignments.json().finally(async () => {
+          await getAssignment().finally(async () => {
+            await getStudentInGroup();
+          });
+        });
+      });
+    } catch (error) {
+      console.log(error);
     }
-    useEffect(() => {
-        getAssignment();
-        getStudentInGroup();
-    }, []);
-    const sort = async() => {
-        //Add students to assignment first
-        try{
-            const body = {"assignmentName": assignmentName}
-            const addAllToAssignment = await fetch(`http://localhost:8000/addAllStudentsToAssignment`, {
-                method:"PUT",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(body)
-            });
-            await addAllToAssignment.json().finally(async() => {
-                //Begin sorting 
-                const sortAssignments = await fetch(`http://localhost:8000/sortAssignments`, {
-                    method:"PUT",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(body)
-                });
-                await sortAssignments.json().finally(async() => {
-                    await getAssignment().finally(async() => {
-                        await getStudentInGroup();
-                    });
-                })
-            })
-        }
-        catch(error){
-            console.log(error)
-        }
-    }
-    //Someone please style this later :-)
-    return(
+  };
+  //Someone please style this later :-)
+  return (
     <div className={background.background}>
-        <div className="four wide column">
-            <Navbar children={["about", "login", "register", "logout"]} />
-            <h1>Assignment : {assignmentName}</h1>
-              <div className={styles.assignmentContainer}>
-              <div className={styles.assignmentGrid}>
-              {groups?.map((group) => (
-                <AssignmentCard assignmentName={group['groupName']} buttonText={"Sort"} isAdmin={true} 
-                description={'Current members: ' + sessionStorage.getItem(group['id'])}/>
+      <div className="four wide column">
+        <Navbar children={["about", "login", "register", "logout"]} />
+        <h1>Assignment : {assignmentName}</h1>
+        <div className={styles.assignmentContainer}>
+          <div className={styles.assignmentGrid}>
+            {groups?.map((group) => (
+              <AssignmentCard
+                assignmentName={group["groupName"]}
+                buttonText={"Sort"}
+                isAdmin={true}
+                description={"Current members: " + sessionStorage.getItem(group["id"])}
+              />
             ))}
             <br></br>
             <Button onClick={() => sort()}>Sort All Groups</Button>
-              </div>
-            </div>
+          </div>
         </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default AssignmentPage;
