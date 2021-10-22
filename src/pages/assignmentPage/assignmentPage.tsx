68f096f781fb1c9791c86318f3c32b87f400a427
aryangupta1 <aryangupta1@outlook.com>
2021-10-22 12:52:15 +1100

    Students join group, admins sort group

--- src/pages/assignmentPage/assignmentPage.tsx
+++ src/pages/assignmentPage/assignmentPage.tsx
@@ -12,6 +12,7 @@ const AssignmentPage = () => {
     const [assignment, setAssignment] = useState<any>();
     //Get assignment name from query param
     const assignmentName = window.location.search.replace('?', '');
+    const buttonText = (sessionStorage.getItem('User') === 'Admin')? 'Sort':'Join';
     const getAssignment = async () =>{
         const email = sessionStorage.getItem('Email');
         const url = (sessionStorage.getItem('User') === 'Student')? `http://localhost:8000/assignments/admin@link.com.au/${assignmentName}`:
@@ -25,7 +26,7 @@ const AssignmentPage = () => {
         //Save group Ids for later
         const groupIds: [] = [];
         const id  = groups?.forEach((group)=>groupIds.push(group['id']));
-        sessionStorage.setItem(`${assignmentName}:NumStudents`, JSON.parse(assignment)[0].students.length)
+        sessionStorage.setItem(`${assignmentName}:NumStudents`, assignmentInfo[0].students.length)
         setId(groupIds);
         
     };
@@ -35,6 +36,7 @@ const AssignmentPage = () => {
             const students = await getStudents.json();
             sessionStorage.setItem(`${id}`, await students);
         })
+        
     }
     useEffect(() => {
         getAssignment();
@@ -67,6 +69,43 @@ const AssignmentPage = () => {
             console.log(error)
         }
     }
+    const cardClick = async () => {
+        try{
+            if(sessionStorage.getItem('User') === 'Student'){
+                //Add student to group first
+                const email = sessionStorage.getItem('Email');
+                const body = {"studentEmail":email, "assignmentName":assignmentName};
+                const addStudentToAssignment = await fetch('http://localhost:8000/addStudentToAssignment',{
+                    method:"PUT",
+                    headers: { "Content-Type": "application/json" },
+                    body:JSON.stringify(body)
+                });
+                await addStudentToAssignment.json()
+                //Join group
+                const selectedGroup = window.location.search.replace('?', '');
+                console.log(selectedGroup)
+                const getGroupId = await fetch(`http://localhost:8000/groups/${selectedGroup}/getId`);
+                const groupId = await getGroupId.text();
+                const reqBody = {"email":email, "id":await groupId};
+                const addToGroup = await fetch(`http://localhost:8000/groupAddition`, {
+                    method:"PUT",
+                    headers: { "Content-Type": "application/json" },
+                    body:JSON.stringify(reqBody)
+                });
+                await addToGroup.json();
+                await window.history.replaceState(null, "React App", `http://localhost:3000/assignment?${assignmentName}`)
+                //get students in group to refresh the card
+                await getStudentInGroup();
+            }
+            else{
+                await window.history.replaceState(null, "React App", `http://localhost:3000/assignment?${assignmentName}`)
+                await sort();
+            }
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
     //Someone please style this later :-)
     return(
     <div className={background.background}>
@@ -76,8 +115,10 @@ const AssignmentPage = () => {
               <div className={styles.assignmentContainer}>
               <div className={styles.assignmentGrid}>
               {groups?.map((group) => (
-                <AssignmentCard assignmentName={group['groupName']} buttonText={"Sort"} isAdmin={true} 
+                <div onClick={() => cardClick()}>
+                <AssignmentCard assignmentName={group['groupName']} buttonText={buttonText} isAdmin={true} 
                 description={'Current members: ' + sessionStorage.getItem(group['id'])}/>
+                </div>
             ))}
             <br></br>
             {sessionStorage.getItem('User') === 'Admin' ?<Button onClick={() => sort()}>Sort All Groups</Button>: null}
